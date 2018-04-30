26ea822721dcfce4baeee0911b73db2dd8ce2630
José Ángel Concepción <jaconcep@Macintosh-de-II1.local>
2018-04-30 18:27:25 +0100

    Finished exercise 3.

--- PR2/MovementsListViewController.swift
+++ PR2/MovementsListViewController.swift
@@ -26,18 +26,38 @@ class MovementsListViewController: UITableViewController {
     
     // BEGIN-UOC-3
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        // Return number of sections
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        // Return number of rows
+        return movements.count
     }
     
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovementCell", for: indexPath) as! MovementCell
+        
+        // Set description
+        cell.descriptionLabel.text = movements[indexPath.row].movementDescription
+        
+        // Set date
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        cell.dateLabel.text = formatter.string(from: movements[indexPath.row].date)
+        
+        // Set amount
+        cell.amountLabel.text = String(format: "%.2f €", (movements[indexPath.row].amount.description as NSString).doubleValue).replacingOccurrences(of: ".", with: ",")
+        
+        // Set red color to negative amounts
+        if(movements[indexPath.row].amount < 0){
+            cell.amountLabel.textColor = UIColor.red;
+        } else {
+            cell.amountLabel.textColor = UIColor.black;
+        }
+        
+        return cell
+    }
     // END-UOC-3
     
     // BEGIN-UOC-5
